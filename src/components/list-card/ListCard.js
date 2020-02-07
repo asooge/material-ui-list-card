1e973f7b0a8810c4a5601b063d138d7d428211dd
asooge <asooge@gmail.com>
2020-02-07 17:55:27 -0500

    Add ListCard.scss
    transfer inline-styles to scss using className 'ui-list-card'

--- src/components/list-card/ListCard.js
+++ src/components/list-card/ListCard.js
@@ -1,35 +1,9 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import { Card as Base } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-const styles = {
-  base: {
-    width: '328px',
-    border: '1px solid #000000',
-    borderRadius: '10px'
-  },
-
-  listContainer: {
-    minHeight: '320px',
-    fontSize: '24px',
-    color: '#OCOCOC',
-    margin: '20px'
-  },
-
-  listItem: {
-    marginBottom: '10px'
-  },
-  
-  header: {
-    display: 'flex',
-    fontSize: '36px',
-    color: '#0C0C0C',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E8E8E8',
-    height: '105px'
-  }
-}
+import './ListCard.scss'
 
 class ListCard extends Component {
   
@@ -37,10 +11,9 @@ class ListCard extends Component {
     const { header, items } = this.props
 
     return (
-      <Base style={styles.base}>
-        <header style={styles.header}>{header}</header>
-        
-        <ul style={styles.listContainer}>
+      <Base className={'ui-list-card'} >
+        <header>{header}</header>
+        <ul>
           {items.map((item, index) => (<li key={index}>{item}</li>))}
         </ul>
       </Base>
